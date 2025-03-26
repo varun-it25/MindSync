fcadaf5220cf4c5e7ad9091ac6d2a1cb0f14f284
varun kangotra <varunkangotra.it25@gmail.com>
2025-03-27 01:22:54 +0530

    hero-section and navbar added

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -1,5 +1,20 @@
-export default function Navbar(){
-    return (
-      <div>Navbar</div>
-    )
+import { Link } from "react-router-dom"
+import { Button } from "./ui/button"
+
+const Navbar = () => {
+  return (
+    <header className="flex justify-center items-center p-4 border-b-2 bg-black sticky top-0">
+      <div className="max-w-6xl flex-1 flex justify-between items-center">
+        <p className="font-semibold text-xl">MindSync</p>
+        <nav className="hidden sm:flex flex-1 justify-center items-center space-x-6">
+          <Link to={`/`} className="hover:font-medium">Home</Link>
+          <Link to={`/about`} className="hover:font-medium">About</Link>
+          <Link to={`/contact`} className="hover:font-medium">Contact</Link>
+        </nav>
+        <Link to={`/`}><Button className="cursor-pointer">Get Started</Button></Link>
+      </div>
+    </header>
+  )
 }
+
+export default Navbar
--- client/src/pages/Landing-Page.tsx
+++ client/src/pages/Landing-Page.tsx
@@ -1,5 +1,24 @@
+import Navbar from "@/components/Navbar";
+import { Button } from "@/components/ui/button";
+import { Link } from "react-router-dom";
+
 export default function Landing_Page(){
   return (
-    <div>Landing Page</div>
+    <div className="w-screen dark bg-black text-accent-foreground">
+      <Navbar />
+      <section className="relative flex justify-center h-[90vh] sm:h-[100%] p-4">
+        <div className="absolute w-full h-full top-0 flex justify-center items-center">
+          <div className="flex flex-col justify-center items-center space-y-3 sm:space-y-5 z-1">
+            <p className="font-bold text-4xl sm:text-5xl">TREAT DEPRESSION</p>
+            <p className="font-semibold text-3xl sm:text-4xl">WITH CHALLENGES</p>
+            <div className="flex space-x-4 p-4">
+              <Link to={`/`}><Button className="cursor-pointer" variant={`outline`}>Learn More</Button></Link>
+              <Link to={`/`}><Button className="cursor-pointer">Get Started</Button></Link>
+            </div>
+          </div>
+        </div>
+        <img src="https://media1.giphy.com/media/v1.Y2lkPTc5MGI3NjExZnJ0OHo2Z2RlOWZ6czJzNGxtbjV6ZWVxcmxrbDBhbmp0NnJheGN5NSZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/lkdIhnHHnFma6xvICt/giphy.gif" className="w-[30.2rem] opacity-50 object-contain" />
+      </section>
+    </div>
   )
 }
